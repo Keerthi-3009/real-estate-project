403b1d1cf129845bf4d0c8e26dce7f3fa41ecf8c
Keerthi-3009 <krithikavimal22@gmail.com>
2026-09-10 11:10:58 +0530

    Add views, urls, and base templates for listings

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Property
 
-# Create your views here.
+def property_list(request):
+    properties = Property.objects.all()
+    return render(request, 'listings/property_list.html', {'properties': properties})
+
+def property_detail(request, pk):
+    property = get_object_or_404(Property, pk=pk)
+    return render(request, 'listings/property_detail.html', {'property': property})
